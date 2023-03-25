ac03763a53965ac68a205f4a48a1797d34595d0b
Roman Zhukov <ro.zhuckov2013@yandex.ru>
2023-03-25 14:41:52 +0300

    изменение стилей слайдера 1.2

--- src/App/UI/carousel/carousel.js
+++ src/App/UI/carousel/carousel.js
@@ -2,6 +2,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './carousel.scss';
+import './carousel.media.scss';
 import { useEffect, useState } from 'react';
 
 const Carousel = (props) => {
@@ -32,16 +33,16 @@ const Carousel = (props) => {
         centerMode:true,
         responsive: [
             {
-              breakpoint: 1470,
+              breakpoint: 1024,
               settings: {
-                slidesToShow: 3,
-                slidesToScroll: 3,
+                slidesToShow: 1,
+                slidesToScroll: 1,
                 infinite: true,
-                dots: true
+                dots: true,
               }
             },
             {
-              breakpoint: 1400,
+              breakpoint: 1460,
               settings: {
                 slidesToShow: 2,
                 slidesToScroll: 2,

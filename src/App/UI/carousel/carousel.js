fab4f25d83409c7d7fc5e48d74712a3308511391
Roman Zhukov <ro.zhuckov2013@yandex.ru>
2023-03-25 15:56:23 +0300

    изменение стилей слайдера 1.6

--- src/App/UI/carousel/carousel.js
+++ src/App/UI/carousel/carousel.js
@@ -2,6 +2,7 @@ import Slider from 'react-slick';
 import "slick-carousel/slick/slick.css"; 
 import "slick-carousel/slick/slick-theme.css";
 import './carousel.scss';
+import './carousel.media.scss';
 import { useEffect, useState } from 'react';
 
 const Carousel = (props) => {
